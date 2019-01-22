0d2f9cfbc6ecab994213b49c55427ac5b5a9c5b2
Veli <vdonmez42@gmail.com>
2019-01-22 17:00:50 +0300

    fix

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,7 @@ import 'firebase/firestore';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCJz4avr0RAFll3F7uEsBezl3aARKl4XQg",
-  authDomain: "vue-chat-d761d.firebaseapp.com",
-  databaseURL: "https://vue-chat-d761d.firebaseio.com",
-  projectId: "vue-chat-d761d",
-  storageBucket: "vue-chat-d761d.appspot.com",
-  messagingSenderId: "1077116031284"
+ 
 };
 
 const firebaseApp = firebase.initializeApp(config);
